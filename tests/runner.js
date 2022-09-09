e12e51dc4923ce802e6205d4bec3735e974a22c9
Fernando Papito <papitorocks@gmail.com>
2022-09-08 21:03:20 -0300

    adicionando o tesults e trocando o banco de dados para QtruckDb3

---
+++ tests/runner.js
@@ -0,0 +1,17 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjZjNDNiNTA4LTA5YTItNGEyZC1hM2YxLTY2ZGE3N2U0Mjg0Yi0xNjYyNjgwMTE4MzI2IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiODQwODRkYjQtMDczYS00Mjg2LWEwNmItYTExN2ZhYTI1Y2NhIiwidHlwZSI6InQifQ.1vbzTPcn66lyZwKBNI0sKzZrCbCKlxugohMr0A50CDs'
+
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: TOKEN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
